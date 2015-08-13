fcf6cdd2a5858482e864470b4872d60dee78b9d1
guoshijie <guoshijie@shinc.net>
2015-08-13 12:01:03 +0800

    增加双击文章id自动填充功能

---
+++ httpclient/src/main/java/net/shinc/test/timmer.java
@@ -0,0 +1,30 @@
+package net.shinc.test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 定时器
+ * @ClassName timmer 
+ * @Description TODO
+ * @author guoshijie 
+ * @date 2015年8月13日 上午10:32:14
+ */
+public class timmer {
+
+	public static void main(String[] args) {
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Hello");
+			}
+			
+		};
+		
+		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+		service.scheduleAtFixedRate(runnable, 5, 3, TimeUnit.SECONDS);
+	}
+
+}
