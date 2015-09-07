0df1eafe5daf4c81b692d9a48311427dcb10edba
zhaozhonglin <zhaozhonglin@shinc.net>
2015-09-07 21:42:49 +0800

    add databinder limit

--- httpclient/src/main/java/net/shinc/common/AbstractBaseController.java
+++ httpclient/src/main/java/net/shinc/common/AbstractBaseController.java
@@ -3,6 +3,8 @@ package net.shinc.common;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import net.shinc.orm.mybatis.bean.common.AdminUser;
 
 import org.springframework.beans.BeansException;
@@ -10,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.MessageSource;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 public abstract class AbstractBaseController implements ApplicationContextAware {
 	
@@ -28,6 +32,11 @@ public abstract class AbstractBaseController implements ApplicationContextAware 
 		this.messageSource = messageSource;
 	}
 
+	@InitBinder
+    protected void initBinder(HttpServletRequest request,
+            ServletRequestDataBinder binder) throws Exception {
+        binder.setAutoGrowCollectionLimit(1024);// 数字越界问题
+    }
 	public IRestMessage getRestMessage() {
 		IRestMessage msg = (IRestMessage)applicationContext.getBean("restMessage");
 		AdminUser currentUser = AdminUser.getCurrentUser();
