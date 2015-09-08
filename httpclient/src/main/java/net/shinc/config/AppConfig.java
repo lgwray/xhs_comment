82d01e9574c10484388ddb46d53c8c266ec366d7
zhangtaichao <taichao1988@foxmail.com>
2015-09-08 10:34:21 +0800

    multithread

--- httpclient/src/main/java/net/shinc/config/AppConfig.java
+++ httpclient/src/main/java/net/shinc/config/AppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.LocaleResolver;
 
 @Configuration
@@ -52,5 +53,19 @@ public class AppConfig {
 		        .build();
 		
 		return httpClient;
+	}
+	
+	@Bean
+	public ThreadPoolTaskExecutor threadPoolExecutor() {
+		ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();  
+		//线程池所使用的缓冲队列  
+		poolTaskExecutor.setQueueCapacity(200);  
+		//线程池维护线程的最少数量  
+		poolTaskExecutor.setCorePoolSize(5);  
+		//线程池维护线程的最大数量  
+		poolTaskExecutor.setMaxPoolSize(1000);  
+		//线程池维护线程所允许的空闲时间  
+		poolTaskExecutor.setKeepAliveSeconds(30000); 
+		return poolTaskExecutor;
 	}
 }
