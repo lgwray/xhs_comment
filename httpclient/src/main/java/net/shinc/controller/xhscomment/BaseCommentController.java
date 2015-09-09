39d94af915f17d48d05bf697323b6aeddb20895c
guoshijie <guoshijie@shinc.net>
2015-09-09 10:01:28 +0800

    多线程初次提交

--- httpclient/src/main/java/net/shinc/controller/xhscomment/BaseCommentController.java
+++ httpclient/src/main/java/net/shinc/controller/xhscomment/BaseCommentController.java
@@ -1,14 +1,16 @@
 package net.shinc.controller.xhscomment;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.validation.Valid;
 
@@ -52,7 +54,7 @@ import com.github.miemiedev.mybatis.paginator.domain.PageList;
 public class BaseCommentController extends AbstractBaseController {
 	
 	@Autowired
-	private ThreadPoolTaskExecutor executor;
+	private ThreadPoolTaskExecutor threadPoolExecutor;
 	
 	@Autowired
 	private CommentServiceImpl commentService;
@@ -64,7 +66,7 @@ public class BaseCommentController extends AbstractBaseController {
 	private BaseCommentService baseCommentService;
 	private static Logger logger = LoggerFactory.getLogger(BaseCommentController.class);
 
-	
+	private int perGroup = 5;
 	
 
 	/**
@@ -328,12 +330,10 @@ public class BaseCommentController extends AbstractBaseController {
 	 */
 	@RequestMapping(value = "/commentIt")
 	@ResponseBody
-		public IRestMessage commentIt(@Valid CommentItForm form) {
+	public IRestMessage commentIt(@Valid CommentItForm form) {
 		IRestMessage msg = getRestMessage();
 		String articleId = form.getArticleId();
-		
 		List<Map> commentList = form.getCommentList();
-		
 		try {
 			for(Iterator<Map> it = commentList.iterator(); it.hasNext();) {
 				Map map = it.next();
@@ -347,56 +347,56 @@ public class BaseCommentController extends AbstractBaseController {
 				try {
 					userId = String.valueOf(AdminUser.getCurrentUser().getId());
 				} catch(Exception e) {
-					
+					logger.error(ExceptionUtils.getStackTrace(e));
 				}
 				jnlService.insertJnlArticleComment(articleId, comment, null, userId, "1");
-				
-				
 				logger.info("评论id:" + articleId + "  map:" + map + "  result:" + re);
 			}
 			msg.setCode(ErrorMessage.SUCCESS.getCode());
 			return msg;
 		} catch(Exception e) {
-			ExceptionUtils.getStackTrace(e);
 			logger.error(ExceptionUtils.getStackTrace(e));
 			return msg;
 		}
-		
 	}
 	
-	@RequestMapping(value = "/commentIt1")
+	@RequestMapping(value = "/commentItMultiThread")
 	@ResponseBody
-	public Callable<IRestMessage> commentItMultiThread(@Valid CommentItForm form) {
+	public IRestMessage commentItMultiThread(@Valid CommentItForm form) {
 		IRestMessage msg = getRestMessage();
 		String articleId = form.getArticleId();
 		List<Map> commentList = form.getCommentList();
+		
 		int size = commentList.size();
-		int count = size % 100 == 0 ? size / 100 : size / 100 + 1;
+		int count = size % perGroup == 0 ? size / perGroup : size / perGroup + 1;
 		
+		List<CommentTask> taskList = new ArrayList<CommentTask>();
+		for(int i = 0; i < count; i++) {
+			int fromIndex = i * perGroup;
+			int toIndex = fromIndex + perGroup;
+			List<Map> sub = toIndex > size ? commentList.subList(fromIndex, size) : commentList.subList(fromIndex, toIndex);
+			CommentTask task = new CommentTask(jnlService, sub, articleId);
+			taskList.add(task);
+		}
 		
-		
-
+		CommentCallable task = new CommentCallable(msg, taskList);
+		Future<IRestMessage> future = threadPoolExecutor.submit(task);
+		try {
+			IRestMessage msg2 = future.get();
+			return msg2;
+		} catch (Exception e) {
+			logger.error(ExceptionUtils.getStackTrace(e));
+		}
+		return msg;
 	}
 	
-	public static void main(String[] args) throws UnsupportedEncodingException {
-//		String str = "%22id%22:%22310%22,%22comment%22:%22%E5%BC%BA%E5%9B%BD%E6%A2%A6%E9%A6%96%E5%85%88%E6%98%AF%E5%BC%BA%E5%86%9B%E6%A2%A6%EF%BC%81%22,%22nick%22:%22xiaoxi940824%22},{%22id%22:%22311%22,%22comment%22:%22%E4%B8%80%E4%B8%AA%E5%85%A8%E4%B8%96%E7%95%8C%E8%BD%B0%E5%8A%A8%E7%9A%84%E5%A4%A7%E5%9E%8B%E9%98%85%E5%85%B5%22,%22nick%22:%22%E5%86%B7%E9%9B%A8%E5%A4%9C01%22},{%22id%22:%22314%22,%22comment%22:%22%E5%9C%A8%E9%98%85%E5%85%B5%E4%B8%AD%E5%90%91%E4%B8%96%E4%BA%BA%E5%B1%95%E7%A4%BA%E5%85%88%E8%BF%9B%E7%9A%84%E6%AD%A6%E5%99%A8%E8%A3%85%E5%A4%87%E6%98%AF%E4%B8%96%E7%95%8C%E5%90%84%E5%9B%BD%E7%9A%84%E9%80%9A%E8%A1%8C%E5%81%9A%E6%B3%95%EF%BC%8C%E8%BF%99%E6%AC%A1%E9%98%85%E5%85%B5%E9%9B%86%E4%B8%AD%E5%B1%95%E7%A4%BA%E4%BA%8684%E7%9A%84%E6%96%B0%E5%9E%8B%E6%AD%A6%E5%99%A8%E8%A3%85%E5%A4%87%EF%BC%8C%E4%BD%93%E7%8E%B0%E4%BA%86%E6%88%91%E5%86%9B%E7%8E%B0%E4%BB%A3%E5%8C%96%E5%BB%BA%E8%AE%BE%E7%9A%84%E5%8F%91%E5%B1%95%E6%B0%B4%E5%B9%B3%EF%BC%8C%E8%BF%99%E6%98%AF%E5%BC%80%E6%94%BE%E9%80%8F%E6%98%8E%E3%80%81%E5%92%8C%E5%B9%B3%E5%8F%8B%E5%A5%BD%E7%9A%84%E4%BD%93%E7%8E%B0%EF%BC%8C%E4%BC%A0%E9%80%92%E7%9A%84%E6%98%AF%E4%B8%8E%E4%B8%96%E7%95%8C%E5%90%84%E5%9B%BD%E4%BA%BA%E6%B0%91%E4%B8%80%E9%81%93%E5%85%B1%E5%90%8C%E7%BB%B4%E6%8A%A4%E4%B8%96%E7%95%8C%E5%92%8C%E5%B9%B3%E7%9A%84%E6%AD%A3%E8%83%BD%E9%87%8F%EF%BC%81%22,%22nick%22:%22%22";
-		String str1 = "data=%7B%22mesgStatus%22%3A%225%22%2C%22messageId%22%3A%22f__-sIzQA04e1xq5%7C3148249%7CAjV8vfwEByVtNH09Uay9PalRexV2YRVM9fPV7WXKtCPD%40apoll%22%7D";
-		String str2 = "data=%7B%22mesgStatus%22%3A%225%22%2C%22messageId%22%3A%22f__-sI0JJzIUOLA8%7C3146984%7CAjV8vfwEByVtNH09Uay9PalRexV2YRVM9fPV7WXKtCPD%40apoll%22%7D";
-		String result1 = URLDecoder.decode(str1,"UTF-8");
-		String result2 = URLDecoder.decode(str2,"UTF-8");
-		
-//		String enc = "这次阅兵集中展示了84%的";
-//		String encResult =URLEncoder.encode(enc);
-		System.out.println(result1);
-		System.out.println(result2);
-//		System.out.println(encResult);
-	}
-
 	class CommentTask implements Callable<Boolean> {
 
 		private JnlServiceImpl jnlservice;
 		private List<Map> list;
 		private String articleId;
+		
+		public CommentTask(){}
 		
 		public CommentTask(JnlServiceImpl jnlService, List<Map> list,String articleId) {
 			this.jnlservice = jnlService;
@@ -405,8 +405,8 @@ public class BaseCommentController extends AbstractBaseController {
 				this.list.addAll(list);
 			}
 			this.articleId = articleId;
-			
 		}
+		
 		@Override
 		public Boolean call() throws Exception {
 			try {
@@ -417,28 +417,22 @@ public class BaseCommentController extends AbstractBaseController {
 					if (StringUtils.isEmpty(nick)) {
 						nick = "新华社客户端网友";
 					}
-					String re = commentService.sendComment("0", this.articleId, comment,
-							nick);
+					String result = commentService.sendComment("0", this.articleId, comment, nick);
 					String userId = "-1";
 					try {
 						userId = String.valueOf(AdminUser.getCurrentUser().getId());
-					} catch (Exception e) {
-
+					} catch(Exception e) {
+						logger.error(ExceptionUtils.getStackTrace(e));
 					}
-					jnlService.insertJnlArticleComment(articleId, comment, null,
-							userId, "1");
-
-					logger.info("评论id:" + articleId + "  map:" + map + "  result:"
-							+ re);
+					jnlService.insertJnlArticleComment(articleId, comment, null, userId, "1");
+					logger.info("评论id:" + articleId + "  map:" + map + "  result:" + result);
 				}
 				return true;
 			} catch (Exception e) {
-				ExceptionUtils.getStackTrace(e);
 				logger.error(ExceptionUtils.getStackTrace(e));
 				return false;
 			}
 		}
-		
 	}
 	
 	class CommentCallable implements Callable<IRestMessage> {
@@ -451,15 +445,82 @@ public class BaseCommentController extends AbstractBaseController {
 		
 		private IRestMessage msg;
 		
+		public CommentCallable(){}
+		
 		public CommentCallable(IRestMessage msg, List<CommentTask> list) {
-			
+			this.msg = msg;
+			this.list = list;
+			this.taskSize = list.size();
 		}
 
 		@Override
 		public IRestMessage call() throws Exception {
-			// TODO Auto-generated method stub
-			return null;
+			Map<Future<Boolean>,Boolean> resmap= new HashMap<Future<Boolean>,Boolean>();
+			for(int i=0; i< taskSize; i++) {
+				 Future<Boolean> future = threadPoolExecutor.submit(list.get(i));
+				 HashMap<Future<Boolean>, Boolean> map = new HashMap<Future<Boolean>,Boolean>();
+				 resmap.put(future, false);
+			}
+			
+			while(taskDone != taskSize) {
+				logger.info("taskSize: " + taskSize + "\ttaskDone: " + taskDone);
+				Set<Future<Boolean>> keySet = resmap.keySet();
+				for (Future<Boolean> future : keySet) {
+					if(!resmap.get(future)) {
+						if(future.isDone()) {
+							taskDone ++ ;
+							resmap.put(future, true);
+						}
+					}
+				}
+			}
+			
+			msg.setCode(ErrorMessage.SUCCESS.getCode());
+			return msg;
+		}
+	}
+	
+	
+	public static void main(String[] args) throws UnsupportedEncodingException {
+//		String str = "%22id%22:%22310%22,%22comment%22:%22%E5%BC%BA%E5%9B%BD%E6%A2%A6%E9%A6%96%E5%85%88%E6%98%AF%E5%BC%BA%E5%86%9B%E6%A2%A6%EF%BC%81%22,%22nick%22:%22xiaoxi940824%22},{%22id%22:%22311%22,%22comment%22:%22%E4%B8%80%E4%B8%AA%E5%85%A8%E4%B8%96%E7%95%8C%E8%BD%B0%E5%8A%A8%E7%9A%84%E5%A4%A7%E5%9E%8B%E9%98%85%E5%85%B5%22,%22nick%22:%22%E5%86%B7%E9%9B%A8%E5%A4%9C01%22},{%22id%22:%22314%22,%22comment%22:%22%E5%9C%A8%E9%98%85%E5%85%B5%E4%B8%AD%E5%90%91%E4%B8%96%E4%BA%BA%E5%B1%95%E7%A4%BA%E5%85%88%E8%BF%9B%E7%9A%84%E6%AD%A6%E5%99%A8%E8%A3%85%E5%A4%87%E6%98%AF%E4%B8%96%E7%95%8C%E5%90%84%E5%9B%BD%E7%9A%84%E9%80%9A%E8%A1%8C%E5%81%9A%E6%B3%95%EF%BC%8C%E8%BF%99%E6%AC%A1%E9%98%85%E5%85%B5%E9%9B%86%E4%B8%AD%E5%B1%95%E7%A4%BA%E4%BA%8684%E7%9A%84%E6%96%B0%E5%9E%8B%E6%AD%A6%E5%99%A8%E8%A3%85%E5%A4%87%EF%BC%8C%E4%BD%93%E7%8E%B0%E4%BA%86%E6%88%91%E5%86%9B%E7%8E%B0%E4%BB%A3%E5%8C%96%E5%BB%BA%E8%AE%BE%E7%9A%84%E5%8F%91%E5%B1%95%E6%B0%B4%E5%B9%B3%EF%BC%8C%E8%BF%99%E6%98%AF%E5%BC%80%E6%94%BE%E9%80%8F%E6%98%8E%E3%80%81%E5%92%8C%E5%B9%B3%E5%8F%8B%E5%A5%BD%E7%9A%84%E4%BD%93%E7%8E%B0%EF%BC%8C%E4%BC%A0%E9%80%92%E7%9A%84%E6%98%AF%E4%B8%8E%E4%B8%96%E7%95%8C%E5%90%84%E5%9B%BD%E4%BA%BA%E6%B0%91%E4%B8%80%E9%81%93%E5%85%B1%E5%90%8C%E7%BB%B4%E6%8A%A4%E4%B8%96%E7%95%8C%E5%92%8C%E5%B9%B3%E7%9A%84%E6%AD%A3%E8%83%BD%E9%87%8F%EF%BC%81%22,%22nick%22:%22%22";
+//		String str1 = "data=%7B%22mesgStatus%22%3A%225%22%2C%22messageId%22%3A%22f__-sIzQA04e1xq5%7C3148249%7CAjV8vfwEByVtNH09Uay9PalRexV2YRVM9fPV7WXKtCPD%40apoll%22%7D";
+//		String str2 = "data=%7B%22mesgStatus%22%3A%225%22%2C%22messageId%22%3A%22f__-sI0JJzIUOLA8%7C3146984%7CAjV8vfwEByVtNH09Uay9PalRexV2YRVM9fPV7WXKtCPD%40apoll%22%7D";
+//		String result1 = URLDecoder.decode(str1,"UTF-8");
+//		String result2 = URLDecoder.decode(str2,"UTF-8");
+		
+//		String enc = "这次阅兵集中展示了84%的";
+//		String encResult =URLEncoder.encode(enc);
+//		System.out.println(result1);
+//		System.out.println(result2);
+//		System.out.println(encResult);
+		
+		int perGroup = 6;
+		List list = new ArrayList();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		
+		int size = list.size();
+		int count = size % perGroup == 0 ? size / perGroup : size / perGroup + 1;
+		
+		for(int i = 0; i < count; i++) {
+			int fromIndex = i * perGroup;
+			int toIndex = fromIndex + perGroup;
+			List sub = toIndex > size ? list.subList(fromIndex, size) : list.subList(fromIndex, toIndex);
+			printList(sub);
 		}
 		
+	}
+	
+	public static void printList(List list) {
+		System.out.println("\n=======begin========");
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+		System.out.println("=======end==========");
 	}
 }
