ba537071c1111a3911c1de781ca188664f8b0039
zhangtaichao <taichao1988@foxmail.com>
2015-09-11 18:36:11 +0800

    commentmapper

--- httpclient/src/main/java/net/shinc/service/xhscomment/impl/JnlCommentServiceImpl.java
+++ httpclient/src/main/java/net/shinc/service/xhscomment/impl/JnlCommentServiceImpl.java
@@ -18,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 @Service
@@ -71,6 +72,7 @@ public class JnlCommentServiceImpl implements JnlCommentService {
 		
 	}
 	
+	@Transactional
 	public List<BatchResult> updateCommentSendFlag(List<JnlComment> paramList) {
 		if(CollectionUtils.isEmpty(paramList)) {
 			throw new IllegalArgumentException();
