fc99c53b251f22d308b1995cac83645ba3a003ab
guoshijie <guoshijie@shinc.net>
2015-10-09 16:06:45 +0800

    update

--- httpclient/src/test/java/net/shinc/service/xhscomment/NickServiceTest.java
+++ httpclient/src/test/java/net/shinc/service/xhscomment/NickServiceTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = InfoMgmtApplication.class)
 @WebAppConfiguration
@@ -26,6 +28,13 @@ public class NickServiceTest {
 	public void getNicksRandom() {
 		List<Nick> list = service.getNicksRandom(100);
 		System.out.println(list.toString());
+	}
+
+	@Test
+	@Transactional
+	public void getNickListByPage2() {
+		PageBounds pageBounds = new PageBounds(Integer.parseInt("1"), Integer.parseInt("50"));
+		service.getNickListByPage2("0", pageBounds);
 	}
 	
 }
