1484aa654ba816e7a1b3a3162da0549b266eabfc
guoshijie <guoshijie@shinc.net>
2015-10-15 17:43:00 +0800

    add mail log

--- httpclient/src/main/java/net/shinc/controller/xhscomment/MailController.java
+++ httpclient/src/main/java/net/shinc/controller/xhscomment/MailController.java
@@ -1,5 +1,7 @@
 package net.shinc.controller.xhscomment;
 
+import java.util.Date;
+
 import javax.mail.Address;
 import javax.mail.internet.InternetAddress;
 
@@ -7,6 +9,7 @@ import net.shinc.common.AbstractBaseController;
 import net.shinc.common.ErrorMessage;
 import net.shinc.common.IRestMessage;
 import net.shinc.service.common.MailService;
+import net.shinc.utils.DateUtils;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
@@ -42,21 +45,36 @@ public class MailController extends AbstractBaseController {
 	public IRestMessage sendEMail() {
 		IRestMessage msg = getRestMessageWithoutUser();
 		try {
-			String fromAddr = "guoshijie@shinc.net";
+			String fromAddr = "gsj_java@163.com";
 			String pwd = "@WSXvfr4";
 			
-			Address[] toAddr = new Address[1];
+			Address[] toAddr = new Address[2];
 			toAddr[0] = new InternetAddress("guoshijie@shinc.net","郭世杰",charset);
-//			toAddr[1] = new InternetAddress("steve_hi@163.com", "郭世杰2",charset);
+			toAddr[1] = new InternetAddress("steve_hi@163.com", "郭世杰2",charset);
 //			toAddr[2] = new InternetAddress("gsj_java@163.com", "郭世杰3",charset);
 			
 			String bccAddr = "steve_hi@163.com";
 			String title = mailService.getMailTitle();
 			String content = mailService.getMailContent();
 			
+			StringBuilder res = new StringBuilder();
+			res.append("发件人: "+fromAddr);
+			
+			res.append("    收件人: ");
+			for (Address address : toAddr) {
+				InternetAddress ia = (InternetAddress)address;
+				String personal = ia.getPersonal();
+				res.append(personal+" ");
+			}
+			
 			mailService.sendMail(fromAddr, pwd, toAddr, null, bccAddr, title, content);
 			msg.setCode(ErrorMessage.SUCCESS.getCode());
+			msg.setResult(res);
+			String now = DateUtils.dateToString(new Date(), "yyyy-MM-dd HH:mm:ss");
+			logger.info("发送时间: "+now+"\t\t状态: 发送成功\t"+res);
 		} catch (Exception e) {
+			String now = DateUtils.dateToString(new Date(), "yyyy-MM-dd HH:mm:ss");
+			logger.info("发送时间: "+now+"\t\t状态: 发送失败");
 			logger.error(ExceptionUtils.getStackTrace(e));
 		}
 		return msg;
