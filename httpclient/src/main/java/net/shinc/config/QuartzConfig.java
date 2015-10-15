9a644ffe416af3226e3a1939001602f3313881c9
guoshijie <guoshijie@shinc.net>
2015-10-15 19:56:31 +0800

    update

--- httpclient/src/main/java/net/shinc/config/QuartzConfig.java
+++ httpclient/src/main/java/net/shinc/config/QuartzConfig.java
@@ -1,15 +1,10 @@
 package net.shinc.config;
 
-import net.shinc.quartz.task.xhscomment.FetchArticleListJob;
-import net.shinc.quartz.task.xhscomment.FetchArticleListJob.FetchArticleListThread;
-import net.shinc.quartz.task.xhscomment.FetchCommentListJob;
-import net.shinc.quartz.task.xhscomment.FetchCommentListJob.FetchCommentListThread;
 import net.shinc.quartz.task.xhscomment.GenerateCommentReportJob;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
@@ -24,76 +19,12 @@ public class QuartzConfig  {
 	@Bean
 	public SchedulerFactoryBean startQuartz() {
 		SchedulerFactoryBean fb = new SchedulerFactoryBean();
-		
-		fb.setTriggers(fetchArticleListJobTrigger().getObject()
-				,fetchCommentListJobTrigger().getObject()
-				,generateCommentReportTrigger().getObject()
+		fb.setTriggers(
+				generateCommentReportTrigger().getObject()
 				);
 		
 		return fb;
 	}
-	
-	/**  FetchArticleListJob begin */
-	@Bean
-	@Scope("prototype")
-	public FetchArticleListThread fetchArticleListThread() {
-		return new FetchArticleListThread();
-	}
-	
-	@Bean
-	public FetchArticleListJob fetchArticleListJob() {
-		return new FetchArticleListJob();
-	}
-	
-	@Bean
-	public MethodInvokingJobDetailFactoryBean fetchArticleListJobDetail() {
-		MethodInvokingJobDetailFactoryBean fb = new MethodInvokingJobDetailFactoryBean();
-		fb.setTargetBeanName("fetchArticleListJob");
-		fb.setTargetMethod("work");
-		fb.setConcurrent(false);
-		return fb;
-	}
-	
-	@Bean
-	public SimpleTriggerFactoryBean fetchArticleListJobTrigger() {
-		SimpleTriggerFactoryBean fb = new SimpleTriggerFactoryBean();
-		fb.setGroup("fetchArticleListJobGroup");
-		fb.setName("fetchArticleListJobTrigger");
-		fb.setJobDetail(fetchArticleListJobDetail().getObject());
-		fb.setRepeatInterval(1000 * env.getProperty("FetchArticleList.intevalSeconds", Integer.class));
-		return fb;
-	}
-	/**  FetchArticleListJob end */
-	
-	/** FetchCommentListJob begin */
-	
-	@Bean
-	@Scope("prototype")
-	public FetchCommentListThread fetchCommentListThread() {
-		return new FetchCommentListThread();
-	}
-	
-	@Bean
-	public FetchCommentListJob fetchCommentListJob() {
-		return new FetchCommentListJob();
-	}
-	@Bean
-	public MethodInvokingJobDetailFactoryBean fetchCommentListJobDetail() {
-		MethodInvokingJobDetailFactoryBean fb = new MethodInvokingJobDetailFactoryBean();
-		fb.setTargetBeanName("fetchCommentListJob");
-		fb.setTargetMethod("work");
-		fb.setConcurrent(false);
-		return fb;
-	}
-	
-	@Bean
-	public SimpleTriggerFactoryBean fetchCommentListJobTrigger() {
-		SimpleTriggerFactoryBean fb = new SimpleTriggerFactoryBean();
-		fb.setJobDetail(fetchCommentListJobDetail().getObject());
-		fb.setRepeatInterval(1000 * env.getProperty("FetchCommentListJob.intevalSeconds", Integer.class));
-		return fb;
-	}
-	/** FetchCommentListJob end */
 	
 	
 	/** generateCommentReportJob begin */
